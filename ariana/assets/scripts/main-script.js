a0fa6c4767484662efcc324189fb910fafd77462
wirash <70929128+wirash@users.noreply.github.com>
2022-12-16 11:05:30 -0300

    16-12-2022 early

--- ariana/assets/scripts/main-script.js
+++ ariana/assets/scripts/main-script.js
@@ -1,5 +1,44 @@
 const main = document.querySelector(".main");
+const profileContainer = document.getElementById("profile-container");
 const postContainer = document.getElementById("post-container");
+const settingsToggle = document.getElementById("settings-toggle");
+const settingsContainer = document.querySelector(".settings-container");
+const settings = document.querySelector(".settings");
+const settingsPaddingTop = document.querySelector(".padding.padding-top");
+const tabContainer = document.querySelector(".tab-container");
+
+let fully_displayed_observer = new IntersectionObserver(
+	(entries) => {
+		entries.forEach((entry) => {
+			if (entry.isIntersecting) {
+				switch (entry.target) {
+					case settingsPaddingTop:
+						settingsContainer.toggleAttribute("open");
+						break;
+					case profileContainer:
+					case postContainer:
+						tabContainer
+							.querySelector(
+								`.tab:nth-child(${entry.target.getAttribute("nthchild")})`
+							)
+							.querySelector("input").checked = true;
+						break;
+				}
+				// if (entry.target == settingsPaddingTop)
+				// 	settingsContainer.toggleAttribute("open");
+			}
+		});
+	},
+	{
+		root: null,
+		rootMargin: "0px",
+		threshold: 1.0,
+	}
+);
+
+fully_displayed_observer.observe(settingsPaddingTop);
+fully_displayed_observer.observe(profileContainer);
+fully_displayed_observer.observe(postContainer);
 
 document.querySelectorAll(".tab").forEach((tab) => {
 	tab.addEventListener(
@@ -14,7 +53,7 @@ document.querySelectorAll(".tab").forEach((tab) => {
 	);
 });
 
-postContainer.scrollIntoView();
+// postContainer.scrollIntoView();
 
 // let last_known_scroll_position = 0;
 // let ticking = false;
@@ -33,3 +72,28 @@ postContainer.scrollIntoView();
 // 	},
 // 	{ passive: true }
 // );
+
+document.querySelectorAll(".close-overlay-btn").forEach((btn) => {
+	btn.onclick = () => {
+		btn.closest(".overlay").removeAttribute("open");
+	};
+});
+
+document
+	.querySelectorAll(".overlay>.padding.padding-middle")
+	.forEach((padding) => {
+		padding.onclick = () => {
+			padding.closest(".overlay").removeAttribute("open");
+		};
+	});
+
+document.querySelectorAll(".context-menu>*").forEach((cmenu) => {
+	cmenu.addEventListener("click", () => {
+		cmenu.closest(".toggle").blur();
+	});
+});
+
+settingsToggle.addEventListener("click", () => {
+	settingsContainer.toggleAttribute("open");
+	settingsContainer.scrollTop = document.body.offsetHeight ?? 0;
+});
